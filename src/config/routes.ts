989a024ef8a570a41e72971ff14dd8eb993c8a80
elleryrain <liquliphy@gmail.com>
2024-10-19 04:54:55 +0300

    add backend routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -6,3 +6,12 @@ export enum routes {
   allProducts = "/products/",
   notFound = "/404",
 }
+
+export enum backendRoutes {
+  categories = "categories/all",
+  categoryById = "categories/:id",
+  products = "products/all",
+  productById = "products/:id",
+  getCouponRequest = "/sale/send",
+  sendOrder = "order/send",
+}

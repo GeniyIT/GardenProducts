b416092011b3b86d26a7270208e02b08f7b45785
Artem Kamaldinov <kamaltema@yandex.ru>
2024-10-22 02:20:39 +0300

    minor commit

--- src/components/FilterProducts/FilterProducts.tsx
+++ src/components/FilterProducts/FilterProducts.tsx
@@ -5,12 +5,16 @@ export function FilterProducts() {
         <div className={styles.filter_products}>
             <div className={styles.filter_products_price}> 
                 <h1 className={styles.filter_products_price_title}>Price</h1>
+                <div className={styles.filter_products_price_container}>
+                    <input className={styles.filter_products_price_input}/> 
             </div>
             <div className={styles.filter_products_discount}> 
-                
+                <h1 className={styles.filter_products_discount_title}>Discount items</h1>
+
             </div>
             <div className={styles.filter_products_sorted}> 
-                
+                <h1 className={styles.filter_products_sorted_title}>Sorted</h1>
+
             </div>
         </div>
     )
